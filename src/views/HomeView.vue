50a61a7dfc772776ffe3d65544a81c0392b0ff83
KLDN <951637214@qq.com>
2025-05-12 09:00:26 +0800

    build: 初始化项目

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Hello Vue3</div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/base.css";
+          @import "@/assets/main.css";
+        `,
+      },
+    },
+  },
+})
